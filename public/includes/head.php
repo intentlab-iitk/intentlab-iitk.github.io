7ee1c931142fc3ce84561073e489f04f5d9f21ad
INTENT Labs IITK <swamyp@iitk.ac.in>
2024-08-28 01:35:41 +0530

    code v1.1.0: Switched index to html
    * For compatibility resasons with git, index is porterd to html.

--- public/includes/head.php
+++
@@ -1,4 +0,0 @@
-<!-- INTENT Labs -->
-    <title>INTENT Labs</title>
-    <link rel="shortcut icon" href="assets/favicon.ico" type="image/x-icon">
-    <link rel="stylesheet" href="styles/main.css">
