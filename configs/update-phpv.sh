60ba9dc0db5cfcc445ac467553d7c7977b9b9ba7
Aravind Potluri <aravindswami135@gmail.com>
2024-12-12 10:38:33 +0530

    code v1.4.1: Bug fix in update phpv
    * It should not restart the server.

--- configs/update-phpv.sh
+++ configs/update-phpv.sh
@@ -1,12 +1,9 @@
 #!/bin/bash
 
-NGINX_CONF="/etc/nginx/sites-available/LabSite.conf"
+NGINX_CONF="/etc/nginx/sites-available/BlackBox.conf"
 
 # Detect the PHP-FPM version
 PHP_FPM_VERSION=$(ls /var/run/php/ | grep -oP 'php[0-9.]+-fpm.sock' | sort -r | head -n 1)
 
 # Update the Nginx configuration
 sudo sed -i "s#fastcgi_pass unix:/var/run/php/.*-fpm.sock;#fastcgi_pass unix:/var/run/php/$PHP_FPM_VERSION;#g" $NGINX_CONF
-
-# Restart Nginx to apply changes
-sudo systemctl restart nginx
